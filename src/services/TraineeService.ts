af27246477a09ffe949bf134ab860f101dcaec8e
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2022-08-28 00:24:50 -0400

    Trainers have Public Address

--- src/services/TraineeService.ts
+++ src/services/TraineeService.ts
@@ -1,6 +1,6 @@
 import Trainee from '../models/Trainee';
 import { database } from '../index';
-import {v4 as uuid} from 'uuid';
+import { v4 as uuid } from 'uuid';
 
 export default class TraineeService {
     async getAllTrainees() {
@@ -36,25 +36,25 @@ export default class TraineeService {
 
     async addTrainee(name: string, interest: string, publicAddress: string) {
         const trainee = Trainee(database);
-        try{
+        try {
             return await trainee.create({
                 id: uuid(),
                 name,
                 interest,
                 publicAddress,
-                trainerId: uuid()
-            })
-        }catch (error) {
-            console.error(error)
-            return []
+                trainerId: uuid(),
+            });
+        } catch (error) {
+            console.error(error);
+            return [];
         }
     }
 
-    async changeName(trainerId: string, newName: string) {
+    async changeName(traineeId: string, newName: string) {
         const traineeTable = Trainee(database);
 
         const where: string[] = [];
-        if (trainerId) where.push(trainerId);
+        if (traineeId) where.push(traineeId);
 
         try {
             const trainee = await traineeTable.findOne({
@@ -78,14 +78,15 @@ export default class TraineeService {
         const traineeTable = Trainee(database);
 
         const where: string[] = [];
-        if (trainerId) where.push(trainerId);
+        if (traineeId) where.push(traineeId);
 
         try {
             const trainee = await traineeTable.findOne({
                 ...where,
             });
-            trainee.trainerId = trainerId;
-            trainee.save({ fields: ['trainerId'] });
+            trainee.update({ trainerId });
+            // trainee.trainerId = trainerId;
+            // trainee.save({ fields: ['trainerId'] });
             return await trainee.reload();
         } catch (error) {
             console.error(error);
